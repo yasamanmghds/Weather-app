bd74c5f1fc816a3bb02bf3efc42c71afaf5ec2dd
yasamanmghds <142737108+yasamanmghds@users.noreply.github.com>
2023-08-27 20:28:29 +0330

    added a new JS file and Fahrenheit conversion

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
-let search = document.getElementById("search-form");
-let currentLoc = document.getElementById("current-location");
-let tempContainer = document.getElementById("cel");
-let cityHeader = document.getElementById("city-name");
-let weather = document.getElementById("weather");
-let windSpeed = document.getElementById("wind-speed");
-let humidity = document.getElementById("humidity");
-
 function getTemp(response) {
+  console.log(response);
   let currentTemp = Math.round(response.data.main.temp);
   tempContainer.innerHTML = currentTemp;
   cityHeader.innerHTML = response.data.name;
   windSpeed.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
-  weather.innerHTML = response.data.weather[0].main;
+  weather.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  icon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchBar(event) {
@@ -40,6 +38,15 @@ function currentPosition(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(currentPositionTemp);
 }
+
+let search = document.getElementById("search-form");
+let currentLoc = document.getElementById("current-location");
+let tempContainer = document.getElementById("cel");
+let cityHeader = document.getElementById("city-name");
+let weather = document.getElementById("weather");
+let windSpeed = document.getElementById("wind-speed");
+let humidity = document.getElementById("humidity");
+let icon = document.getElementById("icon");
 
 currentLoc.addEventListener("click", function () {
   navigator.geolocation.getCurrentPosition(currentPosition);
